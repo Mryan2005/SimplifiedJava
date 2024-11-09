148f8d32c7506d43147af1e5a6195eb47deb0af9
Zhizhong Yan <A2564011261@163.com>
2024-11-09 11:42:19 +0800

    add randInt

--- src/main/java/Random.java
+++ src/main/java/Random.java
@@ -1,5 +1,23 @@
 public class Random {
+    public int RandInt() {
+        java.util.Random rand = new java.util.Random();
+        return rand.nextInt(90000) + 10000;
+    }
+
+    public int RandInt(int min, int max) {
+        java.util.Random rand = new java.util.Random();
+        return rand.nextInt(max - min) + min;
+    }
+
+    public int RandInt(int seed) {
+        java.util.Random rand = new java.util.Random(seed);
+        return rand.nextInt(90000) + 10000;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new java.util.Random().nextInt(90000) + 10000 + "");
+        Random random = new Random();
+        System.out.println(random.RandInt(1, 20));
+        System.out.println(random.RandInt());
+        System.out.println(random.RandInt(123));
     }
 }
