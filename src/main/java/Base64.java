3d9c23bb633e9dc04dd3618e78bfe760b9f6edbf
Zhizhong Yan <A2564011261@163.com>
2024-11-09 10:25:57 +0800

    fix

--- src/main/java/Base64.java
+++ src/main/java/Base64.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class base64 {
+public class Base64 {
+    public String encodeBase64(String input) {
+        return java.util.Base64.getEncoder().encodeToString(input.getBytes());
+    }
+
+    public String decodeBase64(String input) {
+        return new String(java.util.Base64.getDecoder().decode(input));
+    }
+
+    public static void main(String[] args) {
+        Base64 base64 = new Base64();
+        String encoded = base64.encodeBase64("password");
+        System.out.println(encoded);
+        System.out.println(base64.decodeBase64(encoded));
+    }
 }
--- src/main/java/MD5.java
+++ src/main/java/MD5.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class md5 {
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.Random;
+
+public class MD5 {
+    public Hash generateMd5Hash(String input) {
+        Hash hash = new Hash();
+        hash.hash = DigestUtils.md5Hex(input);
+        hash.isSalted = false;
+        return hash;
+    }
+
+    public Hash generateMd5HashPlusSalt(String input) {
+        Hash hash = new Hash();
+        hash.salt = new Random().nextInt(90000) + 10000 + "";
+        hash.hash = DigestUtils.md5Hex(input + hash.salt);
+        hash.isSalted = true;
+        return hash;
+    }
+
+    public boolean compareMd5Hash(String input, String inputSalt, String hash) {
+        return DigestUtils.md5Hex(input + inputSalt).equals(hash);
+    }
+
+    public boolean compareMd5Hash(String input, String hash) {
+        return DigestUtils.md5Hex(input).equals(hash);
+    }
+
+    public static void main(String[] args) {
+        MD5 md5 = new MD5();
+        Hash hash = md5.generateMd5Hash("password");
+        System.out.println(hash.hash);
+        System.out.println(hash.isSalted);
+        Hash hashSalt = md5.generateMd5HashPlusSalt("password");
+        System.out.println(hashSalt.hash);
+        System.out.println(hashSalt.salt);
+        System.out.println(hashSalt.isSalted);
+        System.out.println(md5.compareMd5Hash("password", hash.hash));
+        System.out.println(md5.compareMd5Hash("password", hashSalt.salt, hashSalt.hash));
+    }
 }
